ddc5edf26f21028b42329ed3126846f4cd9d6f00
Jntynho <jntynho@gmail.com>
2026-09-20 21:24:37 +0100

    Update status message handling for video exceptions

--- lib/services/stream_service.dart
+++ lib/services/stream_service.dart
@@ -49,7 +49,7 @@ class StreamProvider {
       } else if (e is VideoUnplayableException) {
         return StreamProvider(
           playable: false,
-          statusMSG: e.reason ?? "Song is unplayable",
+          statusMSG: e.message,
         );
       } else if (e is VideoRequiresPurchaseException) {
         return StreamProvider(
